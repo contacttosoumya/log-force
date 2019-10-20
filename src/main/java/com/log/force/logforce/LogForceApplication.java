8670be4a8046dda987e97425e9865163008bf4b1
Soumya Basak <contacttosoumya@gmail.com>
2019-10-20 13:53:05 -0400

    init code has been committed.

---
+++ src/main/java/com/log/force/logforce/LogForceApplication.java
@@ -0,0 +1,13 @@
+package com.log.force.logforce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogForceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogForceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/log/force/logforce/LogForceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.log.force.logforce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogForceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
